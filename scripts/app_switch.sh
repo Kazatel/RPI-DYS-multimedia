028c0b221f71048dce4483959c4c1d5b01a0d211
tKazatel <tKazatel@gmail.com>
2025-04-27 14:04:56 +0200

    test

--- scripts/app_switch.sh
+++ scripts/app_switch.sh
@@ -4,24 +4,25 @@
 APP="$1"
 
 # Kill current app processes (using pkill -u to only kill user's processes)
-pkill -u $USER -f kodi
-pkill -u $USER -f emulationstation
-pkill -u $USER -f lxsession
+pkill -u "$USER" -f kodi
+pkill -u "$USER" -f emulationstation
+pkill -u "$USER" -f lxsession
 
 # Wait for processes to terminate
-sleep 2
+sleep 3
 
 # Set up environment variables
 export DISPLAY=:0
-export XAUTHORITY=$HOME/.Xauthority
+export XAUTHORITY="$HOME/.Xauthority"
 
 case "$APP" in
   kodi)
-    nohup /usr/bin/kodi --standalone > $HOME/kodi_output.log 2>&1 & ;;
+    setsid /usr/bin/kodi --standalone > "$HOME/kodi_output.log" 2>&1 & ;;
   retropie)
-    nohup /usr/bin/emulationstation > $HOME/retropie_output.log 2>&1 & ;;
+    setsid /usr/bin/emulationstation > "$HOME/retropie_output.log" 2>&1 & ;;
   desktop)
-    nohup /usr/bin/startx > $HOME/desktop_output.log 2>&1 & ;;
+    # If you already have X running, don't call startx, just restart the session manager
+    setsid /usr/bin/lxsession > "$HOME/desktop_output.log" 2>&1 & ;;
   *)
     echo "Usage: app_switch.sh [kodi|retropie|desktop]"
     exit 1 ;;
